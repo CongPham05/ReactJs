685f8652d3c0847c9e5ce5a66a23507a35772054
CongPham <congphamvan115@gmail.com>
2022-08-18 22:46:52 +0700

    18/8

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,70 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss'
+import { getAllUsers } from '../../services/userService';
+
 class UserManage extends Component {
 
-    state = {
-
-    }
-
-    componentDidMount() {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers:[]
+        }
+     }
+ 
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+      
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
     }
 
 
     render() {
+
+        let arrUsers = this.state.arrUsers;
+        console.log(arrUsers)
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className='title text-cencer'>
+                    Manage user with cong pham
+                    <div className='user-table mt-4 mx-3'>
+                        <table id='customers'>
+                            <thead>
+                                <tr>
+                                    <th>Email</th>
+                                    <th>First Name</th>
+                                    <th>Last Name</th>
+                                    <th>Address</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            
+                                 {arrUsers && arrUsers.map((arrUser,index) => {
+                                    return (
+                                        <tr>
+                                            <td key={arrUser.id}>{arrUser.email}</td>
+                                            <td key={arrUser.id}>{arrUser.firstName}</td>
+                                            <td key={arrUser.id}>{arrUser.lastName}</td>
+                                             <td key={arrUser.id}>{arrUser.address}</td>
+                                             <td >
+                                                <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
+                                                <button className='btn-delete'><i className="fas fa-trash"></i></button>
+                                            </td>
+                                        </tr>
+                                        )                                              
+                                })}       
+                            </tbody>
+                    </table>
+                    
+                    </div>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,4 +3,12 @@ import axios from "../axios";
 const handleLoginApi = (email, password) => {
     return axios.post('/api/login',{email, password});
 }
-export { handleLoginApi };
+
+const getAllUsers = (id) => { 
+    return axios.get(`/api/get-all-users?id=${id}`)
+}
+
+export {
+    handleLoginApi,
+    getAllUsers,
+}
